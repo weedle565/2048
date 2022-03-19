925a4b06262d5c3e89fe49c9160bd18d36f9413b
weedle565 <oliverzross@hotmail.com>
2022-03-19 22:45:01 +1100

    Game Good

--- src/main/Game.java
+++ src/main/Game.java
@@ -11,12 +11,16 @@ import java.util.Random;
 
 import static java.awt.Color.*;
 
-public class Game extends JPanel implements ActionListener {
+public class Game extends JPanel {
 
+    //location is used to store the current location of cubes
     private final int[] location = new int[16];
+    //in is used to check if a cube is in a specific location
     private final boolean[] in = new boolean[16];
+    //number inside the cube
     private final int[] num = new int[16];
 
+    //Width and height of a cube
     private final int WIDTH = 144, HEIGHT = 144;
 
     Random rand;
@@ -32,15 +36,20 @@ public class Game extends JPanel implements ActionListener {
 
         rand = new Random();
 
-
+        //set all cubes to 0 and no cubes to be spawning inside the game
         for(int i = 0; i<16; i++){
             location[i] = i;
             in[i] = false;
             num[i] = 0;
         }
 
+        //randomly select starting cubes
         int first = rand.nextInt(0, 15);
         int second = rand.nextInt(0, 15);
+
+        if(second == first){
+            second = rand.nextInt(0, 15);
+        }
 
         in[first] = true;
         in[second] = true;
@@ -48,58 +57,106 @@ public class Game extends JPanel implements ActionListener {
         num[first] = 2;
         num[second] = 2;
 
-        System.out.println(Arrays.toString(location));
-        System.out.println(Arrays.toString(in));
     }
 
-    private void left(){
-        for(int i = 15; i  >= 0; i--){
-            if(in[i] && i < 3 && !in[i+1]){
+    private void left() {
+
+        for (int i = 15; i >= 0; i--) {
+
+            //Check if a cube can move to the left, these if statements check to see where the cube is stopping (the wall or another cube)
+            // First row:
+
+            if (in[i] && i < 3 && !in[i + 1]) {
                 in[i] = false;
-                in[i+1] = true;
+                in[i + 1] = true;
                 System.out.println(i);
-                num[i+1] = num[i];
+                num[i + 1] = num[i];
                 num[i] = 0;
                 repaint();
                 i = 15;
             }
 
-            if(in[i] && i < 7 && i > 3 && !in[i+1]){
+            //Second row
+            if (in[i] && i < 7 && i > 3 && !in[i + 1]) {
                 in[i] = false;
-                in[i+1] = true;
-                num[i+1] = num[i];
-                num[i] = 0;
-                System.out.println(i);
-                repaint();
-                i = 15;
-            }
-
-            if(in[i] && i < 11 && i > 7 && !in[i+1]){
-                in[i] = false;
-                in[i+1] = true;
-                System.out.println(i);
-                num[i+1] = num[i];
+                in[i + 1] = true;
+                num[i + 1] = num[i];
                 num[i] = 0;
                 repaint();
                 i = 15;
             }
 
-            if(in[i] && i < 15 && i > 11 && !in[i+1]){
+            //Third row
+            if (in[i] && i < 11 && i > 7 && !in[i + 1]) {
                 in[i] = false;
-                in[i+1] = true;
-                System.out.println(i);
-                num[i+1] = num[i];
+                in[i + 1] = true;
+                num[i + 1] = num[i];
                 num[i] = 0;
                 repaint();
                 i = 15;
             }
+
+            //Fourth row
+            if (in[i] && i < 15 && i > 11 && !in[i + 1]) {
+                in[i] = false;
+                in[i + 1] = true;
+                num[i + 1] = num[i];
+                num[i] = 0;
+                repaint();
+                i = 15;
+            }
+
+            //Check if 2 cubes moving into each other are equivalent, if yes add together
+            if (in[i] && i < 3 && in[i + 1] && num[i] == num[i + 1]) {
+                num[i + 1] = num[i] * 2;
+                in[i] = false;
+                num[i] = 0;
+                repaint();
+                i = 15;
+            }
+
+            if (in[i] && i > 3 && i < 7 && in[i + 1] && num[i] == num[i + 1]) {
+                num[i + 1] = num[i] * 2;
+                in[i] = false;
+                num[i] = 0;
+                repaint();
+                i = 15;
+            }
+
+            if (in[i] && i > 7 && i < 11 && in[i + 1] && num[i] == num[i + 1]) {
+                num[i + 1] = num[i] * 2;
+                in[i] = false;
+                num[i] = 0;
+                repaint();
+                i = 15;
+            }
+
+            if (in[i] && i > 11 && i < 15 && in[i + 1] && num[i] == num[i + 1]) {
+                num[i + 1] = num[i] * 2;
+                in[i] = false;
+                num[i] = 0;
+                repaint();
+                i = 15;
+            }
+
         }
 
-        System.out.println(Arrays.toString(in));
+        //Create a new cube
+        int newCube = rand.nextInt(0, 15);
+
+        if (!in[newCube]) {
+            in[newCube] = true;
+            num[newCube] = 2;
+            repaint();
+        }
+
+
     }
 
     private void right(){
         for(int i = 15; i > 0; i--) {
+
+            //Check where to stop cubes when moving
             if (in[i]  && !in[i - 1] && i < 4) {
                 in[i] = false;
                 in[i - 1] = true;
@@ -112,7 +169,6 @@ public class Game extends JPanel implements ActionListener {
             if (i < 8 && i > 4 && in[i] && !in[i-1]){
                 in[i] = false;
                 in[i - 1] = true;
-                System.out.println(i);
                 num[i-1] = num[i];
                 num[i] = 0;
                 repaint();
@@ -122,17 +178,48 @@ public class Game extends JPanel implements ActionListener {
             if (i < 12 && i > 8 && in[i] && !in[i-1]){
                 in[i] = false;
                 in[i - 1] = true;
-                System.out.println(i);
                 num[i-1] = num[i];
                 num[i] = 0;
                 repaint();
                 i = 15;
             }
 
+            //Checks if to add the cubes together as they have the same number
+            if(in[i] && i < 4 && in[i-1] && num[i] == num[i-1]){
+                num[i+1] = num[i]*2;
+                in[i] = false;
+                num[i] = 0;
+                repaint();
+                i = 15;
+            }
+
+            if(in[i] && i > 4 &&  i < 8 && in[i-1] && num[i] == num[i-1]){
+                num[i-1] = num[i]*2;
+                in[i] = false;
+                num[i] = 0;
+                repaint();
+                i = 15;
+            }
+
+            if(in[i] && i > 8 &&  i < 12 && in[i-1] && num[i] == num[i-1]){
+                num[i-1] = num[i]*2;
+                in[i] = false;
+                num[i] = 0;
+                repaint();
+                i = 15;
+            }
+
+            if(in[i] && i > 12 &&  i < 16 && in[i-1] && num[i] == num[i-1]){
+                num[i-1] = num[i]*2;
+                in[i] = false;
+                num[i] = 0;
+                repaint();
+                i = 16;
+            }
+
             if (i < 16 && i > 12 && in[i] && !in[i-1]){
                 in[i] = false;
                 in[i - 1] = true;
-                System.out.println(i);
                 num[i-1] = num[i];
                 num[i] = 0;
                 repaint();
@@ -140,10 +227,16 @@ public class Game extends JPanel implements ActionListener {
 
             }
 
-
-
         }
-        System.out.println(Arrays.toString(in));
+
+        //Creates a new cube
+        int newCube = rand.nextInt(0,15);
+
+        if (!in[newCube]) {
+            in[newCube] = true;
+            num[newCube] = 2;
+            repaint();
+        }
     }
 
     private void down(){
@@ -153,25 +246,60 @@ public class Game extends JPanel implements ActionListener {
                 in[i+4] = true;
                 num[i+4] = num[i];
                 num[i] = 0;
-                System.out.println(i);
                 repaint();
                 i = 15;
             }
+
+            if(i < 12 && in[i] && in[i+4] && num[i] == num[i+4]){
+                in[i] = false;
+                in[i+4] = true;
+                num[i+4] = num[i]*2;
+                num[i] = 0;
+                repaint();
+                i = 15;
+            }
+
         }
+        //Creates a new cube
+        int newCube = rand.nextInt(0,15);
+        if (!in[newCube]) {
+            in[newCube] = true;
+            num[newCube] = 2;
+            repaint();
+        }
+
     }
 
     private void up(){
         for(int i = 15; i >= 0; i--){
+
+            if(i > 3 && in[i] && in[i-4] && num[i] == num[i-4]){
+                in[i] = false;
+                in[i+4] = true;
+                num[i+4] = num[i]*2;
+                num[i] = 0;
+                repaint();
+                i = 15;
+            }
+
             if(i > 3 && in[i] && !in[i-4]){
                 in[i] = false;
                 in[i-4] = true;
                 num[i-4] = num[i];
                 num[i] = 0;
-                System.out.println(i);
                 repaint();
                 i = 16;
             }
+
         }
+        //Creates a new cube
+        int newCube = rand.nextInt(0,15);
+        if (!in[newCube]) {
+            in[newCube] = true;
+            num[newCube] = 2;
+            repaint();
+        }
+
     }
 
     @Override
@@ -180,6 +308,7 @@ public class Game extends JPanel implements ActionListener {
         g.setColor(white);
         g.fillRect(0,0,getWidth(),getHeight());
 
+        //Draw cubes (this is foul no judgement)
         for(int i : location)
             if(in[i]) {
                 g.setColor(black);
@@ -251,11 +380,13 @@ public class Game extends JPanel implements ActionListener {
                     }
                 }
            }
-    }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
+        //Destroy the screen on victory cause funny
+        for(int i = 0; i < 16; i++){
+            if(num[i] == 2048){
+                System.exit(1);
+            }
+        }
     }
 
     private class KeyListener extends KeyAdapter{
